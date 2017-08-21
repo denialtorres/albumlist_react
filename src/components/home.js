a18bd275bd19d3e0394deb46ebab7d4cb67babaa
Daniel Torres <denial.torres@gmail.com>
2017-08-21 02:32:51 -0500

    project from album list db

---
+++ src/components/home.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+
+import Banner from '../components/banner'
+import ArtistList from '../components/artistList'
+
+const URL_ARTIST = "http://localhost:3004/artists"
+
+class Home extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+        artist:''
+    }
+
+  }
+
+  componentDidMount(){
+    fetch(URL_ARTIST, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json =>{
+        console.log(json)
+        this.setState({
+          artist: json
+        })
+    })
+  }
+
+
+  render(){
+    return(
+      <div>
+        <Banner />
+        <ArtistList allArtist={this.state.artist}/>
+      </div>
+    )
+  }
+}
+
+export default Home;
